3028b496045a53e42c6d0185f7ba7ee2fb502ba0
ccuts996 <90644291+ccuts996@users.noreply.github.com>
2021-09-13 19:52:49 -0400

    Initial commit

---
+++ src/main/java/ex06.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.time.Year;
+public class ex06 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String age;
+        System.out.print(" What is your age:");
+
+        age = input.next();
+
+        Integer i = Integer.parseInt(age);
+
+        String retireage;
+        System.out.print(" At what age would you want to retire?");
+
+        retireage = input.next();
+
+        Integer j = Integer.parseInt(retireage);
+
+        int currentyear = Year.now().getValue();
+
+        int yearsleft = j - i;
+
+        int retirementyear = currentyear + yearsleft;
+
+        System.out.print(" You have "+ yearsleft + " until you can retire." + System.lineSeparator());
+        System.out.print(" It is "+ currentyear+ " and you can retire in " + retirementyear);
+    }
+}
+
+
+
+
+
